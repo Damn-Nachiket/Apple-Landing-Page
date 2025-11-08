8224a07ace1cd9de59286f1bf422c6e5864232c1
Damn-Nachiket <alphaonfield@gmail.com>
2025-11-09 03:25:07 +1030

    Basic Model Design Setup for 3D Product Viewer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./Components/Hero.jsx"
 import Navbar from "./Components/navbar.jsx"
+import ProductViewer from "./Components/ProductViewer.jsx"
 
 const App = () => {
     return (
         <main>
             <Navbar />
             <Hero />
+            <ProductViewer />
         </main>
     )
 }
