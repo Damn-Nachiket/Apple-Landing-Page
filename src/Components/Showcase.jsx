abca94c944800d1fc7c89e709e68094938bc8a80
Damn-Nachiket <alphaonfield@gmail.com>
2025-11-13 23:31:53 +1030

    Showcase section updated ( Not Responsive )

--- src/Components/Showcase.jsx
+++ src/Components/Showcase.jsx
@@ -1,13 +1,12 @@
-import { useGSAP } from "@gsap/react"
-import gsap from "gsap"
-import { useMediaQuery } from "react-responsive"
+import {useMediaQuery} from "react-responsive";
+import {useGSAP} from "@gsap/react";
+import gsap from 'gsap';
 
 const Showcase = () => {
-
-    const isTablet = useMediaQuery({ query: '(max-width: 1024px' })
+    const isTablet = useMediaQuery({ query: '(max-width: 1024px)'});
 
     useGSAP(() => {
-        if (!isTablet) {
+        if(!isTablet) {
             const timeline = gsap.timeline({
                 scrollTrigger: {
                     trigger: '#showcase',
@@ -18,17 +17,19 @@ const Showcase = () => {
                 }
             });
 
-            timeline.to('.mask img', {
-                transform: 'scale(1.1)'
-            }).to('content', {opacity:1 , y:0, ease: 'power1.in'})
+            timeline
+                .to('.mask img', {
+                    transform: 'scale(1.1)'
+                }).to('.content', { opacity: 1, y: 0, ease: 'power1.in' });
         }
     }, [isTablet])
-  return (
+
+    return (
         <section id="showcase">
             <div className="media">
-                <video src="/videos/game.mp4" loop muted autoPlay playsInline></video>
+                <video src="/videos/game.mp4" loop muted autoPlay playsInline />
                 <div className="mask">
-                    <img src="/mask-logo.svg" alt="M4 Logo" />
+                    <img src="/mask-logo.svg" />
                 </div>
             </div>
 
@@ -41,31 +42,29 @@ const Showcase = () => {
                             <p>
                                 Introducing {" "}
                                 <span className="text-white">
-                                    M4, the next generation of apple silicon
+                                    M4, the next generation of Apple silicon
                                 </span>
-                                .M4 powers
+                                . M4 powers
                             </p>
                             <p>
-                                It can drive apple intelligence on Ipad Pro, so you can write, create, and accomplish more with ease. All in a design unavailably thin, light, and powerful.
+                                It drives Apple Intelligence on iPad Pro, so you can write, create, and accomplish more with ease. All in a design that’s unbelievably thin, light, and powerful.
                             </p>
                             <p>
-                                A brand new display engine delivers breathtaking precision, color accuracy, and brightness. And a next-gen CPU with hardware-accelerated ray tracing bring console-level graphics to you fingerprints.
+                                A brand-new display engine delivers breathtaking precision, color accuracy, and brightness. And a next-gen GPU with hardware-accelerated ray tracing brings console-level graphics to your fingertips.
                             </p>
-                            <p className="text-primary">
-                                Learn more about apple intelligence.
-                            </p>
+                            <p className="text-primary">Learn more about Apple Intelligence</p>
                         </div>
                     </div>
 
                     <div className="max-w-3xs space-y-14">
                         <div className="space-y-2">
                             <p>Up to</p>
-                            <h3>4x Faster</h3>
+                            <h3>4x faster</h3>
                             <p>pro rendering performance than M2</p>
                         </div>
                         <div className="space-y-2">
                             <p>Up to</p>
-                            <h3>1.5x Faster</h3>
+                            <h3>1.5x faster</h3>
                             <p>CPU performance than M2</p>
                         </div>
                     </div>
@@ -74,5 +73,4 @@ const Showcase = () => {
         </section>
     )
 }
-
 export default Showcase
